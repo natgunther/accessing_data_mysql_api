29073b9084fcc351434f377aa9faff3173bc5612
nataliegunther <natgunther@gmail.com>
2024-09-08 11:20:47 -0600

    Adds multi stage docker build Docker compose now runs app and database

--- src/test/java/com/example/accessing_data_mysql/FriendsApplicationTests.java
+++ src/test/java/com/example/accessing_data_mysql/FriendsApplicationTests.java
@@ -1,15 +1,15 @@
-package com.example.accessing_data_mysql;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class FriendsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.accessing_data_mysql;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.annotation.Import;
+//
+//@Import(TestcontainersConfiguration.class)
+//@SpringBootTest
+//class FriendsApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
